9252fbf7975b256616a3c12e7a2d1766edb263d8
Espoir Kakesa <120624185+Kakesa@users.noreply.github.com>
2026-02-03 02:37:25 +0100

    fix : correct the route for superadmin module

--- src/modules/schools/school.utils.js
+++ src/modules/schools/school.utils.js
@@ -1,6 +1,10 @@
 const Counter = require('./school.counter.model');
 
 const generateSchoolCode = async () => {
+  // Générer les deux premiers chiffres aléatoires
+  const randomPrefix = Math.floor(100 + Math.random() * 900); // Génère un nombre entre 100 et 999
+
+  // Récupérer et incrémenter le compteur
   const counter = await Counter.findOneAndUpdate(
     { name: 'school' },
     { $inc: { seq: 1 } },
@@ -10,8 +14,11 @@ const generateSchoolCode = async () => {
     }
   );
 
-  const number = String(counter.seq).padStart(4, '0');
-  return `EG-${number}`;
+  // Formater le numéro de compteur en une chaîne de 2 chiffres
+  const number = String(counter.seq).padStart(2, '0'); // S'assure que le compteur a au moins 2 chiffres
+
+  // Retourner le code d'école avec le préfixe aléatoire et le compteur
+  return `EG-${randomPrefix}-${number}`;
 };
 
 module.exports = {
