254548a964448493986a2166b5aed5869cbe6ab3
Espoir Kakesa <120624185+Kakesa@users.noreply.github.com>
2026-02-04 14:50:17 +0100

    fix : add route subscri.

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,12 @@ const authRoutes = require('./modules/auth/auth.routes');
 const auditRoutes = require('./modules/audit/audit.routes');
 const schoolRoutes = require('./modules/schools/school.routes');
 const superAdminRoutes = require('./modules/superadmin/superadmin.routes');
+const subscriptionRoutes = require('./modules/subscriptions/subscription.routes');
 
 module.exports = (app) => {
   app.use('/api/auth', authRoutes);
   app.use('/api/audit', auditRoutes);
   app.use('/api/schools', schoolRoutes);
   app.use('/api/superadmin', superAdminRoutes);
+  app.use('/api/subscriptions', subscriptionRoutes);
 };
